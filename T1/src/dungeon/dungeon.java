b6c316152980088df8ba98d441c95b05ff2fe50d
francisjssilva <FranciscoSilva@localhost>
2017-02-14 17:06:29 +0000

    matrix done

--- T1/src/dungeon/dungeon.java
+++ T1/src/dungeon/dungeon.java
@@ -6,15 +6,21 @@ public class dungeon {
 	public static void main(String[] args)
 	{
 		Vector<Vector<char[]>> a1= new Vector<Vector<char[]>>();
-		char[][] matrix={
+		char[][] matrix=
+			{
 				{'X','X','X','X','X','X','X','X','X','X'},
 				{'X','H',' ',' ','I',' ','X',' ','G','X'},
 				{'X','X','X',' ','X','X','X',' ',' ','X'},
 				{'X',' ','I',' ','I',' ','X',' ',' ','X'},
-				{'X','X','X',' ','X','X','X',' ',' ','X'}
-				
-				               };
+				{'X','X','X',' ','X','X','X',' ',' ','X'},
+				{'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+				{'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+				{'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X', ' ', 'X'},
+				{'X', ' ', 'I', ' ', 'I', ' ', 'X', 'k', ' ', 'X'},
+				{'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+			};
 		
+		System.out.println(matrix);
 	}
 
 }
