2acf89dc1dfefe2ea5d42e42384c1e69e038b699
up201505092 <up201505092@B03AV1020.salas.feupsig.fe.up.pt>
2017-02-14 16:45:42 +0000

    t1

---
+++ T1/src/dungeon/dungeon.java
@@ -0,0 +1,20 @@
+package dungeon;
+
+import java.util.Vector;
+
+public class dungeon {
+	public static void main(String[] args)
+	{
+		Vector<Vector<char[]>> a1= new Vector<Vector<char[]>>();
+		char[][] matrix={
+				{'X','X','X','X','X','X','X','X','X','X'},
+				{'X','H',' ',' ','I',' ','X',' ','G','X'},
+				{'X','X','X',' ','X','X','X',' ',' ','X'},
+				{'X',' ','I',' ','I',' ','X',' ',' ','X'},
+				{'X','X','X',' ','X','X','X',' ',' ','X'}
+				
+				               };
+		
+	}
+
+}
