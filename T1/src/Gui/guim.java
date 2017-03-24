af3c8492c3a8ae8575404da34f7e893939195ce2
up201505092 <up201505092@I00CS0304.salas.feupsig.fe.up.pt>
2017-03-24 22:39:45 +0000

    iteration 4

--- T1/src/Gui/guim.java
+++ T1/src/Gui/guim.java
@@ -88,6 +88,7 @@ public class guim {
 	private void initialize() 
 	{
 		frame = new JFrame();
+		frame.getContentPane().setFont(new Font("Courier New", Font.PLAIN, 12));
 		frame.setBounds(100, 100, 496, 478);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
@@ -120,6 +121,7 @@ public class guim {
 
 		
 		JButton btnNewGame = new JButton("New Game");
+		btnNewGame.setFont(new Font("Courier New", Font.PLAIN, 12));
 		btnNewGame.addActionListener(new ActionListener() //starts game
 		{ 
 			public void actionPerformed(ActionEvent a)
@@ -130,23 +132,24 @@ public class guim {
 		btnNewGame.setBounds(332, 112, 117, 25);
 		frame.getContentPane().add(btnNewGame);
 		
-		JButton btnNewButton = new JButton("W");
-		btnNewButton.setFont(new Font("Courier 10 Pitch", Font.BOLD, 11));
-		btnNewButton.addActionListener(new ActionListener() //starts game
+		JButton btnW = new JButton("W");
+		btnW.setEnabled(false);
+		btnW.setFont(new Font("Courier New", Font.PLAIN, 11));
+		btnW.addActionListener(new ActionListener() //starts game
 				{ 
 					public void actionPerformed(ActionEvent a)
 					{
 						moveHero('w');
 					}
 				});
-		btnNewButton.setBounds(358, 218, 41, 22);
-		frame.getContentPane().add(btnNewButton);
-		this.w=btnNewButton;
+		btnW.setBounds(358, 218, 41, 22);
+		frame.getContentPane().add(btnW);
+		this.w=btnW;
 		
 
 		
 		JButton btnA = new JButton("A");
-		btnA.setFont(new Font("Courier 10 Pitch", Font.BOLD, 11));
+		btnA.setFont(new Font("Courier New", Font.PLAIN, 11));
 		btnA.addActionListener(new ActionListener() //starts game
 				{ 
 					public void actionPerformed(ActionEvent a)
@@ -160,7 +163,7 @@ public class guim {
 
 		
 		JButton btnD = new JButton("D");
-		btnD.setFont(new Font("Courier 10 Pitch", Font.BOLD, 11));
+		btnD.setFont(new Font("Courier New", Font.PLAIN, 11));
 		btnD.addActionListener(new ActionListener() //starts game
 				{ 
 					public void actionPerformed(ActionEvent a)
@@ -172,14 +175,14 @@ public class guim {
 		frame.getContentPane().add(btnD);
 		this.d=btnD;
 		
-		JLabel label = new JLabel("");
-		label.setBounds(32,451,284,15);
+		JLabel label = new JLabel("Welcome to the Dungeon Game!");
+		label.setBounds(38,414,284,15);
 		label.setFont(new Font ("Courier New",Font.PLAIN,14));
 		frame.getContentPane().add(label);
 		this.label1=label;
 		
 		JButton btnS = new JButton("S");
-		btnS.setFont(new Font("Courier 10 Pitch", Font.BOLD, 11));
+		btnS.setFont(new Font("Courier New", Font.PLAIN, 11));
 		btnS.addActionListener(new ActionListener() //starts game
 				{ 
 					public void actionPerformed(ActionEvent a)
@@ -191,12 +194,13 @@ public class guim {
 		frame.getContentPane().add(btnS);
 		this.s=btnS;
 		
-		this.a.setEnabled(false);
 		this.w.setEnabled(false);
+		this.a.setEnabled(false);
 		this.s.setEnabled(false);
 		this.d.setEnabled(false);
 		
 		JButton btnExit = new JButton("Exit");
+		btnExit.setFont(new Font("Courier New", Font.PLAIN, 12));
 		btnExit.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent a)
@@ -208,7 +212,8 @@ public class guim {
 		frame.getContentPane().add(btnExit);
 		
 		JLabel label1 = new JLabel("");
-		label1.setBounds(35, 415, 284, 15);
+		label1.setFont(new Font("Courier New", Font.PLAIN, 15));
+		label1.setBounds(32, 371, 284, 15);
 		frame.getContentPane().add(label1);
 		
 		
@@ -225,7 +230,10 @@ public class guim {
 		{
 			label1.setText("Invalid move, try again");
 		}
+		else{
+		label1.setText("");
 		level.setGame();
+		}
 		
 		if(!level.gameWin())
 			if(!level.gameOver()){
@@ -246,7 +254,7 @@ public class guim {
 			if(!level.boolLevel2())
 				initLevel2();
 			else{
-				label1.setText("You Won Game! Congratulations!");
+				label1.setText("You Won  the Game! Congratulations!");
 				this.a.setEnabled(false);
 				this.w.setEnabled(false);
 				this.s.setEnabled(false);
@@ -255,6 +263,7 @@ public class guim {
 	}
 
 	private void initLevel2() {    
+		label1.setText("Level 2: The Ogre ");
 		int nOgres = 1;
 		this.a.setEnabled(true);
 		this.w.setEnabled(true);
@@ -285,7 +294,7 @@ public class guim {
 	}
 
 	protected void initLevel1() {
-		label1.setText("Welcome to the dungeon game");
+		label1.setText("Level 1: The Dungeon");
 		this.a.setEnabled(true);
 		this.w.setEnabled(true);
 		this.s.setEnabled(true);
