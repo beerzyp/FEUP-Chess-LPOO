7ec4771d51b17897806360f8b6a886811da65608
Francisco Silva <francisjssilva@gmail.com>
2017-05-23 18:44:56 +0100

    added finnaly the f** proj

---
+++ T2/core/src/com/lpoo/game/Logic/Knights.java
@@ -0,0 +1,46 @@
+package com.lpoo.game.Logic;
+
+/**
+ * Created by FranciscoSilva on 17/05/17.
+ */
+
+public class Knights extends Piece {
+    public Knights(char sym, int pos) {
+        super(sym, pos);
+    }
+
+    public String possibleMove(BoardLogic chessBoard){
+        String list="";
+        char oldPiece;
+        int r=this.posOnBoard/8, c=this.posOnBoard%8;
+        for (int j=-1; j<=1; j+=2) {
+            for (int k=-1; k<=1; k+=2) {
+                try {
+                    if (' ' == chessBoard.getChessBoard()[r+j][c+k*2] || ' ' == chessBoard.getChessBoard()[r+j][c+k*2]) {
+                        oldPiece=chessBoard.getChessBoard()[r+j][c+k*2];
+                        chessBoard.getChessBoard()[r][c]=' ';
+                        if (chessBoard.kingSafe()) {
+                            list=list+r+c+this.getSymbol()+(r+j)+(c+k*2)+oldPiece+';';
+                        }
+                        chessBoard.getChessBoard()[r][c]='K';
+                        chessBoard.getChessBoard()[r+j][c+k*2]=oldPiece;
+                    }
+                } catch (Exception e) {}
+                try {
+                    if (Character.isLowerCase(chessBoard.getChessBoard()[r+j*2][c+k]) || ' ' == chessBoard.getChessBoard()[r+j*2][c+k]) {
+                        oldPiece=chessBoard.getChessBoard()[r+j*2][c+k];
+                        chessBoard.getChessBoard()[r][c]=' ';
+                        if (chessBoard.kingSafe()) {
+                            list=list+r+c+this.getSymbol()+(r+j*2)+(c+k)+oldPiece+';';
+                        }
+                        chessBoard.getChessBoard()[r][c]='K';
+                        chessBoard.getChessBoard()[r+j*2][c+k]=oldPiece;
+                    }
+                } catch (Exception e) {}
+            }
+        }
+
+        this.actualPossibleMoves = list;
+        return list;
+    }
+}
